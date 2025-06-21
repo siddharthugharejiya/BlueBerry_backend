50327828b3e9b8284402a271d5fcea595272742c
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2025-06-21 10:06:53 +0530

    .

--- config/Server.js
+++ config/Server.js
@@ -2,8 +2,12 @@ import mongoose from "mongoose";
 import 'dotenv/config'
 export const MainServer = async () => {
     try {
-        await mongoose.connect(process.env.MONGOOSE_URL)
-        console.log("server is running");
+        await mongoose.connect(process.env.MONGOOSE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+            .then(() => console.log("MongoDB Connected"))
+            .catch((err) => console.log("MongoDB Connection Error:", err));
     } catch (error) {
         console.log("server is not responde")
     }
