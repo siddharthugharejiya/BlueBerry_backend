fb2a7dae8246cc3abf08fcc4a7bf6696e7a7982d
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2025-06-26 11:59:18 +0530

    .

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const app = express();
 app.use(helmet())
 app.use(express.json())
 app.use(cors({
-    origin: "https://frontend-weld-mu.vercel.app",
+    origin: ["https://frontend-weld-mu.vercel.app","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
-}));
+}))
 
 app.use("/", UserRouter)
 app.use("/", ProductRouter)
